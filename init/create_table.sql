452d027fb8389a19d8c75d9d045d4db6843a2c59
Kota-2021 <kota.lalala.2021@gmail.com>
2025-12-17 19:15:46 +0900

    feag:Dockerファイル削除。docker-compose修正。sql追加。

---
+++ init/create_table.sql
@@ -0,0 +1,165 @@
+create table m_categories (
+	category_id SERIAL PRIMARY KEY,
+	category_name varchar(5) NOT NULL
+);
+
+create table m_subjects (
+	subject_code smallint PRIMARY KEY,
+	subject_name varchar(20) NOT NULL
+);
+
+create table m_books (  
+	book_code smallint PRIMARY KEY,
+	book_name varchar(6) NOT NULL
+);
+
+CREATE TABLE m_budgets (
+	budget_id SERIAL PRIMARY KEY,
+	subject_code  smallint NOT NULL,
+	fiscal_year   smallint NOT NULL,
+	budget   numeric(10,2) NOT NULL,
+	category_id   integer,
+
+	CONSTRAINT fk_m_budgets_category
+			FOREIGN KEY (category_id)
+			REFERENCES m_categories(category_id)
+			ON UPDATE CASCADE
+			ON DELETE SET NULL
+);
+
+create table t_cashbook (
+	cashbook_id SERIAL PRIMARY KEY,
+	cashbook_date date NOT NULL,
+	item varchar(20) NOT NULL,
+	withdrawal decimal NOT NULL,
+	deposit decimal NOT NULL,
+	balance decimal NOT NULL,
+	remarks varchar(20) NOT NULL,
+	book_code smallint,
+
+	CONSTRAINT fk_t_cashbook_book
+		FOREIGN KEY (book_code)
+		REFERENCES m_books(book_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL
+);
+
+create table t_journal (
+	journal_id SERIAL PRIMARY KEY,
+	journal_date date NOT NULL,
+	withdrawal decimal NOT NULL,
+	deposit decimal NOT NULL,
+	subject_code smallint,
+	item varchar(20) NOT NULL,
+	customer varchar(20) NOT NULL,
+	evidence varchar(6) NOT NULL,
+	memo varchar(20) NOT NULL,
+	book_code smallint,
+	category_id integer,
+	fiscal_year smallint NOT NULL,
+
+	CONSTRAINT fk_t_journal_subject
+		FOREIGN KEY (subject_code)
+		REFERENCES m_subjects(subject_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+
+	CONSTRAINT fk_t_journal_book
+		FOREIGN KEY (book_code)
+		REFERENCES m_books(book_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+	
+	CONSTRAINT fk_t_journal_category
+		FOREIGN KEY (category_id)
+		REFERENCES m_categories(category_id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL
+);
+
+create table t_receivable (
+	receivable_id SERIAL PRIMARY KEY,
+	receivable_date date NOT NULL,
+	withdrawal decimal NOT NULL,
+	deposit decimal NOT NULL,
+	subject_code smallint,
+	item varchar(20) NOT NULL,
+	customer varchar(20) NOT NULL,
+	evidence varchar(6) NOT NULL,
+	memo varchar(20) NOT NULL,
+	book_code smallint,
+	category_id integer,
+	fiscal_year smallint NOT NULL,
+
+	CONSTRAINT fk_t_receivable_subject
+		FOREIGN KEY (subject_code)
+		REFERENCES m_subjects(subject_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+
+	CONSTRAINT fk_t_receivable_book
+		FOREIGN KEY (book_code)
+		REFERENCES m_books(book_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+
+	CONSTRAINT fk_t_receivable_category
+		FOREIGN KEY (category_id)
+		REFERENCES m_categories(category_id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL
+);
+
+create table t_payable (
+	payable_id SERIAL PRIMARY KEY,
+	payable_date date NOT NULL,
+	withdrawal decimal NOT NULL,
+	deposit decimal NOT NULL,
+	subject_code smallint,
+	item varchar(20) NOT NULL,
+	customer varchar(20) NOT NULL,
+	evidence varchar(6) NOT NULL,
+	memo varchar(20) NOT NULL,
+	book_code smallint,
+	category_id integer,
+	fiscal_year smallint NOT NULL,
+
+	CONSTRAINT fk_t_payable_subject
+		FOREIGN KEY (subject_code)
+		REFERENCES m_subjects(subject_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+
+	CONSTRAINT fk_t_payable_book
+		FOREIGN KEY (book_code)
+		REFERENCES m_books(book_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+
+	CONSTRAINT fk_t_payable_category
+		FOREIGN KEY (category_id)
+		REFERENCES m_categories(category_id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL
+);
+
+create table t_financial_data (
+	financial_data_id SERIAL PRIMARY KEY,
+	subject_code smallint,
+	budget decimal NOT NULL,
+	result decimal NOT NULL,
+	category_id integer,
+	fiscal_year smallint NOT NULL,
+
+	CONSTRAINT fk_t_financial_data_subject
+		FOREIGN KEY (subject_code)
+		REFERENCES m_subjects(subject_code)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL,
+
+	CONSTRAINT fk_t_financial_data_category
+		FOREIGN KEY (category_id)
+		REFERENCES m_categories(category_id)
+		ON UPDATE CASCADE
+		ON DELETE SET NULL
+);
